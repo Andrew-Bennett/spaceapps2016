c895f3df549fbd2be65be676e738b8b7423c1821
tomjonandy <Thomas@nas10-240-32-61.york.ac.uk>
2016-04-23 14:44:31 +0100

    khgjh

--- src/DataGrabber.java
+++ src/DataGrabber.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class DataGrabber {
@@ -12,6 +13,21 @@ public class DataGrabber {
 			doc = Jsoup.connect("http://en.wikipedia.org/").get();
 			Elements newsHeadlines = doc.select("#mp-itn b a");
 			System.out.println(newsHeadlines);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Document doc2;
+		try {
+			doc2 = Jsoup.connect("http://www.flightstats.com/go/FlightStatus/flightStatusByFlight.do?airline=BA&flightNumber=0105").userAgent("Mozilla").data("name", "jsoup").get();
+			Elements route = doc2.select(".route");
+			Elements flight = doc2.select("#mainAreaLeftColumn");
+			Element info = doc2.select("table").get(4);
+			System.out.println(route);
+			System.out.println(flight);
+			System.out.println(info);
+		
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
