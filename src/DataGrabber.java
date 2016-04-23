8ede339bdfba265b96f4477dc943eb36c7691130
Andrew-Bennett <andrew@nas10-240-44-146.york.ac.uk>
2016-04-23 15:32:07 +0100

    j

--- src/DataGrabber.java
+++ src/DataGrabber.java
@@ -8,6 +8,8 @@ import org.jsoup.select.Elements;
 public class DataGrabber {
 
 	public static void main(String[] cheese) {
+<<<<<<< HEAD
+<<<<<<< HEAD
 		Document doc;
 		try {
 			doc = Jsoup.connect("http://en.wikipedia.org/").get();
@@ -18,16 +20,36 @@ public class DataGrabber {
 			e.printStackTrace();
 		}
 
+=======
+=======
+>>>>>>> master
+		
+>>>>>>> master
 		Document doc2;
+		String airline = "BA";
+		String number = "0105";
 		try {
-			doc2 = Jsoup.connect("http://www.flightstats.com/go/FlightStatus/flightStatusByFlight.do?airline=BA&flightNumber=0105").userAgent("Mozilla").data("name", "jsoup").get();
+			doc2 = Jsoup.connect(String.format("http://www.flightstats.com/go/FlightStatus/flightStatusByFlight.do?airline=%s&flightNumber=%s", airline, number)).userAgent("Mozilla").data("name", "jsoup").get();
 			Elements route = doc2.select(".route");
+<<<<<<< HEAD
+<<<<<<< HEAD
 			Elements flight = doc2.select("#mainAreaLeftColumn flightName");
 			Element info = doc2.select("table").get(4);
 			System.out.println(route);
 			System.out.println(flight);
 			System.out.println(info);
 
+=======
+=======
+>>>>>>> master
+			Elements flight = doc2.select("td.statusValue");
+			Element dep = flight.select("td").get(0);
+			Element arr = flight.select("td").get(1);
+			System.out.println("Flight route: " + route.text());
+			System.out.println("Scheduled departure time: " + dep.text());
+			System.out.println("Scheduled arrival time: " + arr.text());
+		
+>>>>>>> master
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
