26b1b691198e483d2c45e46bbe128d69f830635c
Andrew-Bennett <andrew@nas10-240-44-146.york.ac.uk>
2016-04-23 17:32:25 +0100

    a

--- src/DataGrabber.java
+++ src/DataGrabber.java
@@ -21,14 +21,15 @@ public class DataGrabber {
 		String airline = "BA";
 		String number = "0105";
 		try {
-			doc2 = Jsoup.connect(String.format("http://www.flightstats.com/go/FlightStatus/flightStatusByFlight.do?airline=%s&flightNumber=%s", airline, number)).userAgent("Mozilla").data("name", "jsoup").get();
-			Elements route = doc2.select(".route");
+			doc2 = Jsoup.connect(String.format("http://www.flightstats.com/go/Airport/weather.do?airport=%s", airport)).userAgent("Take-off").data("name", "jsoup").get();
 			Elements flight = doc2.select("td.statusValue");
 			Element dep = flight.select("td").get(0);
 			Element arr = flight.select("td").get(1);
-			System.out.println("Flight route: " + route.text());
-			System.out.println("Scheduled departure time: " + dep.text());
-			System.out.println("Scheduled arrival time: " + arr.text());
+			System.out.println(dep)
+
+			// System.out.println("Flight route: " + route.text());
+			// System.out.println("Scheduled departure time: " + dep.text());
+			// System.out.println("Scheduled arrival time: " + arr.text());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
